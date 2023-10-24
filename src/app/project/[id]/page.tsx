5b900fdbd0c9622c532adcca46cabf4eb1e48b25
Hanjun Kwon <kwon96718@gmail.com>
2023-10-25 04:20:12 +0900

    test: test kv, blob

--- src/app/project/[id]/page.tsx
+++ src/app/project/[id]/page.tsx
@@ -1,13 +1,16 @@
 import { MDXRemote } from 'next-mdx-remote/rsc';
-import { md } from '_temp/projectdetail';
+import { kv } from '@vercel/kv';
 import { containerCss } from '@/styles/project.detail.css';
 
-export default function Page({ params }: { params: { id: string } }) {
+type ResponseType = { title: string; frontTag: string[]; backTag: string[]; content: string };
+export default async function Page({ params }: { params: { id: string } }) {
   const { id } = params;
+  const res = await kv.hgetall<ResponseType>('60f3f8ea-7cc4-48b7-b1f0-fb9bdbbb71a9');
+
   return (
     <div className={containerCss}>
       {id}
-      <MDXRemote source={md} />
+      <MDXRemote source={(res && res.content) || 'error'} />
     </div>
   );
 }
