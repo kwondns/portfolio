04c61cc26eca03d5824b499d274b9269f1870d8e
Hanjun Kwon <kwon96718@gmail.com>
2023-10-29 12:08:53 +0900

    style: root page image -> span

--- src/app/_styles/app.css.ts
+++ src/app/_styles/app.css.ts
@@ -1,24 +1,5 @@
-import { style } from '@vanilla-extract/css';
+import { globalStyle, style } from '@vanilla-extract/css';
 import { vars } from '@/styles/globalTheme.css';
-
-export const imageCss = style({
-  position: 'fixed',
-  top: '60px',
-  left: 0,
-  width: '100vw',
-  height: '100vh',
-  backgroundImage: 'url("/images/main.png")',
-  backgroundSize: 'contain',
-  backgroundRepeat: 'no-repeat',
-  backgroundPosition: 'center',
-  '@media': {
-    'screen and (max-width:700px)': {
-      backgroundImage: 'url("/images/main_mobile.png")',
-    },
-  },
-  // zIndex: 11,
-  animation: 'fadeInUp 0.2s ease',
-});
 
 export const containerCss = style({
   position: 'fixed',
@@ -27,5 +8,36 @@ export const containerCss = style({
   width: '100vw',
   height: '100vh',
   backgroundColor: vars.color.dark,
-  // zIndex: 10,
+  display: 'flex',
+  justifyContent: 'center',
+});
+
+export const codeContainerCss = style({
+  display: 'flex',
+  flexDirection: 'column',
+  rowGap: '4px',
+  margin: '16px auto',
+  maxWidth: '100%',
+});
+
+export const captionCss = style({
+  color: '#747474',
+  fontSize: '1rem !important',
+});
+
+// export const delayCss =
+export const orangeColorCss = style({ color: '#c88570' });
+export const yellowColorCss = style({ color: '#FFC66D' });
+export const greenColorCss = style({ color: '#50A255' });
+export const whiteColorCss = style({ color: '#A7A7A7' });
+export const blueColorCss = style({ color: '#3397E7' });
+
+export const firstCss = style({ paddingLeft: '2ch' });
+export const secondCss = style({ paddingLeft: '4ch' });
+export const thirdCss = style({ paddingLeft: '6ch' });
+export const fourthCss = style({ paddingLeft: '8ch' });
+
+globalStyle(`${codeContainerCss} > span`, {
+  fontSize: '1.1rem',
+  marginLeft: '20px',
 });
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,53 @@
-import { containerCss, imageCss } from '@/styles/app.css';
+import {
+  blueColorCss,
+  captionCss,
+  codeContainerCss,
+  containerCss,
+  firstCss,
+  fourthCss,
+  greenColorCss,
+  orangeColorCss,
+  secondCss,
+  thirdCss,
+  whiteColorCss,
+  yellowColorCss,
+} from '@/styles/app.css';
 
 export default function Page() {
   return (
-    <>
-      <div className={containerCss} />
-      <div className={imageCss} />
-    </>
+    <div className={containerCss}>
+      <div className={codeContainerCss}>
+        <span className={captionCss}>1 usage new *</span>
+        <span className={orangeColorCss}>
+          export default function<span className={yellowColorCss}>{` Me() {`}</span>
+        </span>
+        <span className={`${orangeColorCss} ${firstCss}`}>
+          return<span className={yellowColorCss}> (</span>
+        </span>
+        <span className={`${yellowColorCss} ${secondCss}`}>{`<KwonHanJun>`}</span>
+        <span className={`${greenColorCss} ${thirdCss}`}>
+          {`<짧은 소개>`}
+          <span className={whiteColorCss}>최신 기술 트렌드를 받아들이고 적용하고자 하는 노력형 개발자.</span>
+          {`</짧은 소개>`}
+        </span>
+        <span className={`${greenColorCss} ${thirdCss}`}>{`<하고 싶은 일>`}</span>
+        <span className={`${blueColorCss} ${fourthCss}`}>{`<FrontEnd-Developer />`}</span>
+        <span className={`${blueColorCss} ${fourthCss}`}>{`<BackEnd-Developer />`}</span>
+        <span className={`${blueColorCss} ${fourthCss}`}>{`<FullStack-Developer />`}</span>
+        <span className={`${greenColorCss} ${thirdCss}`}>{`</하고싶은일>`}</span>
+        <span className={`${greenColorCss} ${thirdCss}`}>{`<MyStack>`}</span>
+        <span className={`${blueColorCss} ${fourthCss}`}>{`<React.JS />`}</span>
+        <span className={`${blueColorCss} ${fourthCss}`}>{`<Next.JS />`}</span>
+        <span className={`${blueColorCss} ${fourthCss}`}>{`<Express.JS />`}</span>
+        <span className={`${blueColorCss} ${fourthCss}`}>{`<MongoDB />`}</span>
+        <span className={`${blueColorCss} ${fourthCss}`}>{`<ETC />`}</span>
+        <span className={`${greenColorCss} ${thirdCss}`}>{`</MyStack>`}</span>
+        <span className={`${yellowColorCss} ${secondCss}`}>{`</KwonHanJun>`}</span>
+        <span className={`${yellowColorCss} ${firstCss}`}>
+          )<span className={orangeColorCss}>;</span>
+        </span>
+        <span className={yellowColorCss}>{`}`}</span>
+      </div>
+    </div>
   );
 }
