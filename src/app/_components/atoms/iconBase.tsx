754cff17135c086a1dd7cc7b944943737926b227
Hanjun Kwon <kwon96718@gmail.com>
2023-12-26 13:43:14 +0900

    style: eslint
    - disable react/require-default-props

--- src/app/_components/atoms/iconBase.tsx
+++ src/app/_components/atoms/iconBase.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
 type IconBaseProps = {
+  // eslint-disable-next-line react/require-default-props
   className?: React.ReactNode;
   children: React.ReactNode;
 };
 export default function IconBase(props: IconBaseProps) {
-  const { className, children } = props;
+  const { className = '', children } = props;
   return <span className={`material-symbols-outlined ${className}`}>{children}</span>;
 }
