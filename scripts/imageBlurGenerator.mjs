acef0c3e5c18e7b8f30daa5fe664b370c362ad17
Hanjun Kwon <kwon96718@gmail.com>
2023-12-31 23:26:06 +0900

    feat: create blur object path
    - imageBlurGenerator.mjs에서 blur데이터가 저장될 위치의 디렉토리 생성
    - 위 작업이 없을 시 배포 환경에서 해당 경로가 없어 에러

--- scripts/imageBlurGenerator.mjs
+++ scripts/imageBlurGenerator.mjs
@@ -1,18 +1,19 @@
 import { getPlaiceholder } from 'plaiceholder';
-import {readFile,readdir, writeFile} from 'node:fs/promises'
+import { mkdir, readFile, readdir, writeFile } from 'node:fs/promises';
 
 const generateBlur = async () => {
+  mkdir('src/app/_constants', { recursive: true });
   const files = await readdir(`public/images`, { recursive: true });
-  const blurObject = {}
+  const blurObject = {};
   for (const filePath of files) {
-    if (filePath.endsWith(".png")) {
-      const fullPath = `public/images/${filePath}`
-      const buffer = await readFile(fullPath)
-      const { ...plaiceholder } = await getPlaiceholder(buffer)
-      blurObject[`/images/${filePath}`] = plaiceholder.base64
+    if (filePath.endsWith('.png')) {
+      const fullPath = `public/images/${filePath}`;
+      const buffer = await readFile(fullPath);
+      const { ...plaiceholder } = await getPlaiceholder(buffer);
+      blurObject[`/images/${filePath}`] = plaiceholder.base64;
     }
   }
-  await writeFile('src/app/_constants/blur.ts', `const blurObject:{[key:string]:string}=${JSON.stringify(blurObject)};export default blurObject`)
-}
+  await writeFile('src/app/_constants/blur.ts', `const blurObject:{[key:string]:string}=${JSON.stringify(blurObject)};export default blurObject`);
+};
 
-generateBlur()
+generateBlur();
