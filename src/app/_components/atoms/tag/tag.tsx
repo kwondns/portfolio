90f24d6aa09b818fa32e4fe5fe774e905c3b0775
Hanjun Kwon <kwondns@gmail.com>
2024-01-05 00:09:05 +0900

    style: tag.tsx 불필요한 코드 제거
    - NoteTagColorType 이라는 불필요한 타입을 지우고 string 으로 변경

--- src/app/_components/atoms/tag/tag.tsx
+++ src/app/_components/atoms/tag/tag.tsx
@@ -1,10 +1,10 @@
 import { assignInlineVars } from '@vanilla-extract/dynamic';
-import { NoteTagColorType, TagStackType, TagType } from '@/types/stackType';
+import { TagStackType, TagType } from '@/types/stackType';
 import { noteTagColor, noteTagCss, spanCss, tagActiveCss, tagCss } from '@/components/atoms/tag/tag.css';
 import SpanBase from '@/components/atoms/base/spanBase';
 
 // eslint-disable-next-line react/require-default-props
-type TagProps = TagType & { type: TagStackType | NoteTagColorType; isNote?: boolean; isActive?: boolean };
+type TagProps = TagType & { type: TagStackType | string; isNote?: boolean; isActive?: boolean };
 export default function Tag(props: TagProps) {
   const { type, value, isNote = false, isActive = false } = props;
   return (
