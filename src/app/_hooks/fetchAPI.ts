4ce516a4a65e4e654a43b2b4a371e3edd61a9acc
Hanjun Kwon <kwondns@gmail.com>
2024-01-14 13:38:54 +0900

    test: Api URL

--- src/app/_hooks/fetchAPI.ts
+++ src/app/_hooks/fetchAPI.ts
@@ -1,5 +1,5 @@
 let requestURL: string;
-if (process.env.PUBLIC_NEXT_VERCEL_URL) requestURL = `https://${process.env.PUBLIC_NEXT_VERCEL_URL}/api/`;
+if (process.env.PUBLIC_NEXT_VERCEL_URL) requestURL = `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/`;
 else requestURL = 'http://localhost:3000/api/';
 
 export default async function fetchAPI<T>(url: string): Promise<T> {
