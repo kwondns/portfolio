8d01e54ab970ace98376a4437a85c5bed0d8903a
Hanjun Kwon <kwondns@gmail.com>
2024-01-14 09:34:28 +0900

    fix: Note API 수정
    - API내부의 여러 함수에서 GET이 아닌 함수에 export를 사용하여 빌드 시 에러
    - 해당 문제를 export 제거로 해결

--- src/app/api/note-page/route.ts
+++ src/app/api/note-page/route.ts
@@ -4,25 +4,25 @@ import { query } from '../notion';
 
 const notion = new Client({ auth: process.env.NEXT_PUBLIC_NOTION_KEY });
 
-export async function AllNote() {
+async function AllNote() {
   const response = await notion.databases.query(query.all());
   const responseResult = response.results;
   return NextResponse.json(responseResult);
 }
 
-export async function NoteByTag(tag: string) {
+async function NoteByTag(tag: string) {
   const response = await notion.databases.query(query.oneFilter({ property: 'Tags', multi_select: { contains: tag } }));
   const responseResult = response.results;
   return NextResponse.json(responseResult);
 }
 
-export async function NoteBySearch(search: string) {
+async function NoteBySearch(search: string) {
   const response = await notion.databases.query(query.oneFilter({ property: 'Name', title: { contains: search } }));
   const responseResult = response.results;
   return NextResponse.json(responseResult);
 }
 
-export async function NoteByMulti(tag: string, search: string) {
+async function NoteByMulti(tag: string, search: string) {
   const response = await notion.databases.query(
     query.multiFilter([
       { property: 'Name', title: { contains: search } },
