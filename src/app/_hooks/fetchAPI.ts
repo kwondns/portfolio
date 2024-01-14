7017136cc1105514d5949b98114009e40d38d636
Hanjun Kwon <kwondns@gmail.com>
2024-01-14 13:00:14 +0900

    fix: Vercel api url
    - Vercel Deploy 환경에서의 URL 환경변수 이름 수정

--- src/app/_hooks/fetchAPI.ts
+++ src/app/_hooks/fetchAPI.ts
@@ -1,5 +1,5 @@
 let requestURL: string;
-if (process.env.NEXT_PUBLIC_VERCEL_URL) requestURL = `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/`;
+if (process.env.VERCEL_URL) requestURL = `https://${process.env.VERCEL_URL}/api/`;
 else requestURL = 'http://localhost:3000/api/';
 
 export default async function fetchAPI<T>(url: string): Promise<T> {
