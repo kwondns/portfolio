fb51a71637dd09de67071262c0301a0c86569167
Hanjun Kwon <kwondns@gmail.com>
2024-01-14 15:32:24 +0900

    feat: note

--- src/app/note/page.tsx
+++ src/app/note/page.tsx
@@ -1,16 +1,16 @@
-// import fetchAPI from '@/hooks/fetchAPI';
-// import NoteTemplate from '@/components/templates/note/noteTemplate';
+import fetchAPI from '@/hooks/fetchAPI';
+import NoteTemplate from '@/components/templates/note/noteTemplate';
 import { containerCss } from '@/styles/layout.css';
 import Container from '@/components/atoms/base/container';
-// import { NoteType, TagsType } from '@/types/note';
+import { NoteType, TagsType } from '@/types/note';
 
 export default async function Page() {
-  // const notes = await fetchAPI<NoteType[]>('note-page');
-  // const tags = await fetchAPI<TagsType[]>('note-tag');
+  const notes = await fetchAPI<NoteType[]>('note-page');
+  const tags = await fetchAPI<TagsType[]>('note-tag');
 
   return (
     <Container className={containerCss} id="note">
-      {/* <NoteTemplate notes={notes} tags={tags} /> */}
+      <NoteTemplate notes={notes} tags={tags} />
     </Container>
   );
 }
