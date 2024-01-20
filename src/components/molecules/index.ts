5261e24f25728c8ca88b9cf908ea457f19b7e500
Hanjun Kwon <kwondns@gmail.com>
2024-01-20 22:13:09 +0900

    🎨 Style: 각 디렉토리 별 index.ts 생성
    - 각 디렉토리 별 모듈의 import, export를 한 곳에서 관리
    - 해당 모듈을 사용하는 모듈의 import를 깔끔하게 관리
    - ex) import { Container, ProfileTypo } from '@/atoms'
    - 한줄에 어느 디렉토리의 어느 모듈을 추가하였는지 파악 용

---
+++ src/components/molecules/index.ts
@@ -0,0 +1,5 @@
+export { default as Stepper } from './Stepper';
+export { default as ProfileInfo } from './ProfileInfo';
+export { default as ProfileIntro } from './ProfileIntro';
+export { default as StackByCategory } from './StackByCategory';
+export { default as Menu } from './Menu';
---
+++ src/components/organisms/index.ts
@@ -0,0 +1,1 @@
+export { default as StackByTech } from './StackByTech';
