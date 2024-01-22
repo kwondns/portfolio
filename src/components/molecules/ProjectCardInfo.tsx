0b003a73135441f39dbe78ef11221265d99a55ef
Hanjun Kwon <kwondns@gmail.com>
2024-01-23 07:23:24 +0900

    💄 Design: ProjectCardInfo
    - 전체적인 gap-y를 줄이고 글자 영역의 mb를 늘림
    - ProjectTags를 ProjectDetail에서 사용시 gap이 필요하여 적용
    - 기존의 ProjectCardInfo에서 사용되던 gap-y로 간격이 크게 작용
    - gap-y 줄여 전체적인 차이를 줄이고 mb로 태그 부분만 하위로 이동

--- src/components/molecules/ProjectCardInfo.tsx
+++ src/components/molecules/ProjectCardInfo.tsx
@@ -1,5 +1,6 @@
-import { Container, Div, Tag, Typo } from '@/atoms';
+import { Div, Typo } from '@/atoms';
 import { ProjectType } from '@/types';
+import { ProjectTags } from '@/molecules';
 
 type ProjectCardTagProps = {
   project: Omit<ProjectType.ProjectType, 'preview_image'>;
@@ -8,23 +9,13 @@ export default function ProjectCardInfo(props: ProjectCardTagProps) {
   const { project } = props;
   const { title, date, db, front_tag: frontTag, back_tag: backTag, shorten_content: shortenContent } = project;
   return (
-    <Div className="absolute inset-x-0 bottom-0 flex h-auto max-h-20 flex-col gap-y-2 bg-gray-200/80 px-4 py-2 backdrop-blur-sm transition-all hover:overflow-y-auto group-hover:max-h-full dark:bg-gray-600/80">
+    <Div className="absolute inset-x-0 bottom-0 flex h-auto max-h-20 flex-col gap-y-1 bg-gray-200/80 px-4 py-2 backdrop-blur-sm transition-all hover:overflow-y-auto group-hover:max-h-full dark:bg-gray-600/80">
       <Typo className="text-xl">{title}</Typo>
-      <Div className="mb-2 flex justify-between">
+      <Div className="mb-3 flex justify-between">
         <Typo>{shortenContent}</Typo>
         <Typo className="text-sm">{date}</Typo>
       </Div>
-      <Container type="projectTag">
-        {frontTag.map((front) => (
-          <Tag key={front} color="front" value={front} />
-        ))}
-      </Container>
-      <Container type="projectTag">
-        {backTag.map((back) => (
-          <Tag key={back} color="back" value={back} />
-        ))}
-      </Container>
-      <Tag color="etc" value={db} />
+      <ProjectTags frontTag={frontTag} backTag={backTag} db={db} />
     </Div>
   );
 }
