f59e1acbd3d3986991e98682d04ab9826177dc94
Hanjun Kwon <kwondns@gmail.com>
2024-01-25 07:11:40 +0900

    🚑️ Fix: Project Detail Image 수정
    - 경로 재설정

--- src/components/md/img.tsx
+++ src/components/md/img.tsx
@@ -1,6 +1,12 @@
 import { Img } from '@/atoms';
 
 export default function img(props: any) {
-  const { node, ...others } = props;
-  return <Img className="mx-auto my-4 max-h-[400px] w-auto max-w-[600px]" {...others} />;
+  const { node, src, ...others } = props;
+  return (
+    <Img
+      className="mx-auto my-4 max-h-[400px] w-auto max-w-[600px]"
+      src={`${import.meta.env.VITE_SUPABASE_BUCKET_URL}/${src}`}
+      {...others}
+    />
+  );
 }
