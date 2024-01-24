0b942ba3e7715b761d3a5268e6190e7a32290490
Hanjun Kwon <kwondns@gmail.com>
2024-01-25 04:20:02 +0900

    🚀 Feat: Project Card Loading 추가
    - useQuery를 통해 얻은 isLoading으로 로딩 상태
    - <Spinner> 추가

--- src/components/templates/ProjectTemplate.tsx
+++ src/components/templates/ProjectTemplate.tsx
@@ -9,12 +9,12 @@ import { ProjectCard } from '@/organisms';
 
 export default function ProjectTemplate() {
   const initialData = useLoaderData() as LoaderType.LoaderDataType<typeof projectLoader>;
-  const { data } = useQuery({ ...useProject.useProjectAll(), initialData });
+  const { data, isLoading } = useQuery({ ...useProject.useProjectAll(), initialData });
   if (data === undefined) return <Typo>오류가 발생했습니다.</Typo>;
   return (
     <Div className="flex flex-col items-center px-4">
       {data.map((project) => (
-        <ProjectCard key={project.id} project={project} />
+        <ProjectCard key={project.id} project={project} isLoading={isLoading} />
       ))}
     </Div>
   );
