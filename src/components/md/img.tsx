1f179e8439566cabb065df654ee4930eee0e4bb9
Hanjun Kwon <kwondns@gmail.com>
2024-01-27 04:49:09 +0900

    💄 Design: MD img 스타일 수정
    - max-width를 md(768px)이상에서 작동하도록 수정

--- src/components/md/img.tsx
+++ src/components/md/img.tsx
@@ -4,7 +4,7 @@ export default function img(props: any) {
   const { node, src, ...others } = props;
   return (
     <Img
-      className="mx-auto my-4 max-h-[400px] w-auto max-w-[600px]"
+      className="mx-auto my-4 max-h-[400px] w-auto md:max-w-[600px]"
       src={`${import.meta.env.VITE_SUPABASE_BUCKET_URL}/${src}`}
       {...others}
     />
