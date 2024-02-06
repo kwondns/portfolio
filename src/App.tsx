a95aa2ccc6354dca232e93b9a870ec8e3e2b279c
Hanjun Kwon <kwondns@gmail.com>
2024-02-06 17:57:05 +0900

    🚀 Feat: GSAP 연결
    - GSAP을 사용할 수 있도록 연결
    - useGSAP과 ScrollTrigger 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import { RecoilRoot } from 'recoil';
 import { useEffect } from 'react';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { useGSAP } from '@gsap/react';
 
 import Router from './router';
 
 function App() {
+  gsap.registerPlugin(useGSAP, ScrollTrigger);
   useEffect(() => {
     const setVh = () => {
       document.documentElement.style.setProperty('--vh', `${window.innerHeight}px`);
