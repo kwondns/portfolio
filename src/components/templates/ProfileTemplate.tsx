32ed00f32569aa50ab2df792112b894a117f1944
Hanjun Kwon <kwondns@gmail.com>
2024-02-08 19:02:57 +0900

    🚀 Feat: GSAP 애니메이션 생성
    - ScrollTrigger를 통해 스크롤의 위치에 따라 애니메이션 생성

--- src/components/templates/ProfileTemplate.tsx
+++ src/components/templates/ProfileTemplate.tsx
@@ -1,13 +1,30 @@
+import { useGSAP } from '@gsap/react';
+import { useRef } from 'react';
+import { gsap } from 'gsap';
+
 import { Typo } from '@/atoms';
 import { Stepper, ProfileInfo, ProfileIntro } from '@/molecules';
 
 export default function ProfileTemplate() {
+  const sectionRef = useRef<HTMLDivElement | null>(null);
+  useGSAP(
+    () => {
+      gsap.utils.toArray<Element>('.card').forEach((element) => {
+        gsap
+          .timeline({
+            scrollTrigger: { trigger: element, start: '100% 100%', end: '100% 100%', scrub: true },
+          })
+          .fromTo(element, { opacity: 0, y: 100 }, { opacity: 1, y: 0, duration: 3 }, 0);
+      });
+    },
+    { scope: sectionRef },
+  );
   return (
-    <article className="mx-auto flex flex-col " id="profile">
-      <Typo className="text-5xl">Kwon HanJun</Typo>
+    <section className="mx-auto mt-32 flex min-h-screen flex-col" id="profile" ref={sectionRef}>
+      <Typo className="ml-10 text-6xl">Kwon HanJun</Typo>
       <ProfileInfo />
       <Stepper />
       <ProfileIntro />
-    </article>
+    </section>
   );
 }
--- src/components/templates/ProjectTemplate.tsx
+++ src/components/templates/ProjectTemplate.tsx
@@ -1,21 +1,46 @@
 import { useLoaderData } from 'react-router-dom';
 import { useQuery } from '@tanstack/react-query';
+import { useGSAP } from '@gsap/react';
+import { gsap } from 'gsap';
+import { useRef } from 'react';
 
 import { LoaderType } from '@/types';
 import { projectLoader } from '@/pages';
 import { useProject } from '@/hooks';
-import { Div, Typo } from '@/atoms';
+import { Typo } from '@/atoms';
 import { ProjectCard } from '@/organisms';
 
 export default function ProjectTemplate() {
   const initialData = useLoaderData() as LoaderType.LoaderDataType<typeof projectLoader>;
   const { data, isLoading } = useQuery({ ...useProject.useProjectAll(), initialData });
+  const sectionDiv = useRef<HTMLDivElement | null>(null);
+  useGSAP(
+    () => {
+      gsap.utils.toArray<Element>('.project-card').forEach((element) => {
+        gsap
+          .timeline({
+            scrollTrigger: {
+              trigger: element,
+              start: '50% 20%',
+              end: '20% 0%',
+              scrub: 1,
+            },
+          })
+          .to(
+            element,
+            { transform: 'rotateX(-10deg) scale(0.9)', transformOrigin: 'top', filter: 'brightness(0.3)' },
+            0,
+          );
+      });
+    },
+    { scope: sectionDiv },
+  );
   if (data === undefined) return <Typo>오류가 발생했습니다.</Typo>;
   return (
-    <Div className="flex flex-col items-center px-4">
+    <section className="perspective-[1000px] flex flex-col items-center pb-80" ref={sectionDiv}>
       {data.map((project) => (
         <ProjectCard key={project.id} project={project} isLoading={isLoading} />
       ))}
-    </Div>
+    </section>
   );
 }
