d0cd1d44f70b2a29f7e412e076961786949524c4
kwondns <155633601+kwondns@users.noreply.github.com>
2024-03-16 19:05:48 +0900

    🚀 Feat: Markdown 변경
    - 타 프로젝트에서 사용중인 마크다운과 통일하여 변경

--- src/components/md/table.tsx
+++ src/components/md/table.tsx
@@ -1,4 +1,6 @@
 export default function table(props: any) {
   const { node, ...others } = props;
-  return <table className="mx-auto my-2 table-auto border-separate border border-slate-500 p-2" {...others} />;
+  return (
+    <table className="mx-auto my-2 table-auto border-separate border border-slate-500 p-2 text-stone-300" {...others} />
+  );
 }
