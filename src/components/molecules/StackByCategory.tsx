577992d54e0366dffc78040051f14eca64bb7792
kwondns <155633601+kwondns@users.noreply.github.com>
2024-03-25 19:01:40 +0900

    💄 Design: Stack Style 수정
    - 카테고리와 Stack 버튼의 간격 조

--- src/components/molecules/StackByCategory.tsx
+++ src/components/molecules/StackByCategory.tsx
@@ -5,10 +5,9 @@ type StackByCategoryProps = StackType.StackCategoryType;
 export default function StackByCategory(props: StackByCategoryProps) {
   const { category, img, url, name } = props;
   return (
-    <Div className="grid grid-rows-[40px_1fr] gap-2 md:gap-4 md:p-4">
-      <Typo className="pl-3 text-base text-slate-600 dark:text-slate-300 md:text-2xl">{category}</Typo>
-      {/* <Div className="grid auto-rows-fr grid-cols-3 gap-4 sm:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 "> */}
-      <Div className="flex flex-wrap gap-2 sm:gap-3 md:gap-4">
+    <Div className="grid grid-rows-[40px_1fr] md:gap-4 md:p-4">
+      <Typo className="pl-3 text-base self-end text-slate-600 dark:text-slate-300 md:text-2xl">{category}</Typo>
+      <Div className="mb-2 flex flex-wrap gap-2 sm:gap-3 md:gap-4">
         {name.map((stackName, index) => (
           <Stack key={stackName} name={stackName} img={img[index]} url={url[index]} />
         ))}
