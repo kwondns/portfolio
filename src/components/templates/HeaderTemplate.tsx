efd0a0ac2ee988c0bd3a8e9847dd4016c9eefdeb
kwondns <kwondns@gmail.com>
2025-01-26 16:46:16 +0900

    ♻️ [Refactor]: Light 모드 제거
    - 임시로 dark로만 제공

--- src/components/templates/HeaderTemplate.tsx
+++ src/components/templates/HeaderTemplate.tsx
@@ -1,10 +1,10 @@
 import { Menu } from '@/molecules';
-import { ThemeButton } from '@/atoms';
+// import { ThemeButton } from '@/atoms';
 
 export default function HeaderTemplate() {
   return (
-    <header className="fixed inset-x-0 top-0 z-[10000] mb-4 flex items-center justify-around px-1 py-2 backdrop-blur-2xl md:px-4 md:py-3">
-      <ThemeButton />
+    <header className="fixed inset-x-0 top-0 z-[10000] mb-4 mr-8 flex items-center justify-end px-1 py-2 backdrop-blur-2xl md:px-4 md:py-3">
+      {/* <ThemeButton /> */}
       <Menu />
     </header>
   );
