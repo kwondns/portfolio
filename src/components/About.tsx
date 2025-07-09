3e4d53a30a8bda81f24faa47bac2d81a8601a87c
kwondns <kwondns@gmail.com>
2025-07-09 23:47:39 +0900

    ♻️ [Refactor]: Timeline 상수화
    - `TIMELINE` 상수를 생성하여 타임라인 데이터를 `timeline.ts`로 분리
    - 코드 가독성 개선 및 유지보수성 강화

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -6,6 +6,7 @@ import Github from '@/public/github.svg';
 import Typography from '@/components/Typography';
 import TimelineContent from '@/components/TimelineContent';
 import Timeline from '@/components/Timeline';
+import TIMELINE from '@/constants/timeline';
 
 export default function About() {
   return (
@@ -36,15 +37,9 @@ export default function About() {
         </BentoCard>
       </BentoGrid>
       <Timeline>
-        <TimelineContent
-          year="2021"
-          content="AI를 활용한 기계 절삭력 예측"
-          company="한국생산기술연구원"
-          role="연구원"
-        />
-        <TimelineContent year="2022" content="Web 생태계 이해" company="SPASS" role="인턴 개발자" />
-        <TimelineContent year="2023" content="컴퓨터정보통신공학과 학사" company="홍익대학교" role="졸업" />
-        <TimelineContent year="2024 - 2025" content="마이레저 웹앱 출시" company="세레머니" role="풀스택 개발자" />
+        {TIMELINE.map(({ year, content, company, role }) => (
+          <TimelineContent key={content} year={year} content={content} company={company} role={role} />
+        ))}
       </Timeline>
     </section>
   );
---
+++ src/constants/timeline.ts
@@ -0,0 +1,6 @@
+export default [
+  { year: '2021', content: 'AI를 활용한 기계 절삭력 예측', company: '한국생산기술연구원', role: '연구원' },
+  { year: '2022', content: 'Web 생태계 이해', company: 'SPASS', role: '인턴 개발자' },
+  { year: '2023', content: '컴퓨터정보통신공학과 학사', company: '홍익대학교', role: '졸업' },
+  { year: '2024 - 2025', content: '마이레저 웹앱 출시', company: '세레머니', role: '풀스택 개발자' },
+];
