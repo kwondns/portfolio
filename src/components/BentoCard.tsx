9e6e29649227f325f1769da8f25d309910ed9897
kwondns <kwondns@gmail.com>
2025-07-13 00:52:00 +0900

    💄 [Design]: BentoCard 배경색 스타일 변경
    - `BentoCard` 컴포넌트의 배경색을 `bg-secondary-active`로 수정
    - 기존 배경색 대비 강조된 스타일 제공 및 UI 일관성 향상

--- src/components/BentoCard.tsx
+++ src/components/BentoCard.tsx
@@ -7,7 +7,7 @@ export default function BentoCard(props: BentoCardProps) {
   const { children, className, noGap = false } = props;
   return (
     <div
-      className={`flex flex-col ${noGap ? '' : 'gap-2'} bg-surface p-4 rounded-2xl border-card-border border-[1px] ${className ?? ''}`}
+      className={`flex flex-col ${noGap ? '' : 'gap-2'} bg-secondary-active p-4 rounded-2xl border-border border-[1px] ${className ?? ''}`}
     >
       {children}
     </div>
