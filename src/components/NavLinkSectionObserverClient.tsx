2554dab22e0ba87ee77ca1ca90559ca7ff265910
kwondns <kwondns@gmail.com>
2025-07-13 00:40:59 +0900

    🚀 [Feat]: Observer를 활용한 액티브 섹션 감지 구현
    - `NavLinkSectionObserverClient` 컴포넌트 생성
      - `IntersectionObserver` 활용, 현재 보이는 섹션을 감지하여 상태 업데이트
      - 활성화된 섹션에 스타일 적용
    - `Header` 컴포넌트 업데이트
      - 기존 `NavLink` 제거, 새 컴포넌트로 대체
      - 섹션의 활성 상태에 따라 동적인 스타일 변경
    - 스크롤 감지 임계값 변경, UX를 고려한 자연스러운 동작 향상

---
+++ src/components/NavLinkSectionObserverClient.tsx
@@ -0,0 +1,41 @@
+'use client';
+import Link from 'next/link';
+import { useEffect } from 'react';
+
+export default function NavLinkSectionObserverClient({
+  section,
+  isActive,
+  action,
+}: {
+  section: string;
+  isActive: boolean;
+  action: (section: string) => void;
+}) {
+  useEffect(() => {
+    const id = section.toLowerCase();
+    const sectionElement = document.getElementById(id);
+    if (!sectionElement) return;
+
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          action(section);
+        }
+      },
+      { rootMargin: '0px', threshold: 0.3 },
+    );
+
+    observer.observe(sectionElement);
+    return () => {
+      observer.disconnect();
+    };
+  }, []);
+  return (
+    <Link
+      href={`/#${section.toLowerCase()}`}
+      className={`hover:text-primary-hover text-sm sm:text-lg sm:px-2 focus:ring-4 focus:ring-primary/40 focus:outline-none rounded-lg ${isActive ? 'text-primary' : ''}`}
+    >
+      {section}
+    </Link>
+  );
+}
