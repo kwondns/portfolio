263f4c864474fdcc8cd8f707946d3bf4f53c08b4
kwondns <kwondns@gmail.com>
2025-07-13 23:40:53 +0900

    🚀 [Feat]: LockScrollModalClient 기능 개선 및 접근성 향상
    - 키보드 ESC 입력으로 모달 닫기 기능 추가
    - `children` 타입을 `ReactNode`에서 `React.ReactNode`로 변경
    - 모달 외부 클릭 방지 영역 클래스 수정 및 접근성을 위한 `aria-label="모달"` 추가
    - 닫기 버튼 배경 스타일 개선 및 hover/active 상태 추가
    - 네이티브 스크롤 이벤트를 활용해 내부 콘텐츠 영역의 스크롤 동작 최적화

--- src/components/LockScrollModalClient.tsx
+++ src/components/LockScrollModalClient.tsx
@@ -1,12 +1,12 @@
 'use client';
 
-import { ReactNode, useEffect } from 'react';
+import { useEffect } from 'react';
 import DynamicIcon from '@/components/DynamicIcon';
 
 interface Props {
   open: boolean;
   action: () => void;
-  children: ReactNode;
+  children: React.ReactNode;
 }
 
 export default function LockingModal({ open, action, children }: Props) {
@@ -28,20 +28,31 @@ export default function LockingModal({ open, action, children }: Props) {
     };
   }, [open]);
 
+  const onEscape = (e: KeyboardEvent) => {
+    if (e.key === 'Escape') action();
+  };
+
+  useEffect(() => {
+    window.addEventListener('keydown', onEscape);
+    return () => {
+      window.removeEventListener('keydown', onEscape);
+    };
+  }, []);
+
   if (!open) return null;
   return (
-    <div className="fixed inset-0 bg-background/50 flex items-center justify-center" onClick={action}>
+    <div className="fixed inset-0 bg-background/50 flex items-center justify-center" aria-label="모달" onClick={action}>
       <div
-        className="relative border-[1px] border-focus-ring bg-surface rounded-lg w-11/12 max-w-[1400px] max-h-[85dvh] overflow-y-scroll modal-container"
+        className="relative border-[1px] border-focus-ring bg-surface rounded-lg w-11/12 max-w-[1400px] max-h-[85dvh]"
         onClick={(e) => e.stopPropagation()}
       >
         <button
-          className="absolute text-center top-4 right-4 z-50 leading-0 text-white/60 hover:text-white:80 active:text-white hover:bg-info/80 active:bg-info rounded-full p-1"
+          className="absolute text-center top-4 right-4 z-50 leading-0 text-white/60 hover:text-white:80 active:text-white bg-info/50 hover:bg-info/80 active:bg-info rounded-full p-1"
           onClick={action}
         >
           <DynamicIcon name="close" />
         </button>
-        {children}
+        <div className="overflow-y-scroll overflow-x-auto max-h-[85dvh] modal-container rounded-b-lg">{children}</div>
       </div>
     </div>
   );
