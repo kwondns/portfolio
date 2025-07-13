3de2327b46bf3215f75ad78b3a51f506e03e5e9f
kwondns <kwondns@gmail.com>
2025-07-13 23:49:15 +0900

    💄 [Design]: Footer 컴포넌트 Typography 적용
    - `Typography` 컴포넌트를 사용해 Footer 텍스트 스타일 정리
    - 기존 `p` 태그를 제거하고 `Typography`로 교체
    - 텍스트 클래스 및 스타일 속성 간소화하여 가독성과 유지보수성 향상

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,11 @@
+import Typography from '@/components/Typography';
+
 export default function Footer() {
   return (
-    <footer className="mx-4 py-4">
-      <p className="text-info place-self-center">&copy; 2025 권한준 (Kwon HanJun). All rights reserved.</p>
+    <footer className="mx-4 py-4 justify-self-center">
+      <Typography color="info" type="small" className="place-self-center">
+        &copy; 2025 권한준 (Kwon HanJun). All rights reserved.
+      </Typography>
     </footer>
   );
 }
