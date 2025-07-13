8d3605279fe6e1883bc8b76f8a61720738fa81ce
kwondns <kwondns@gmail.com>
2025-07-13 22:32:14 +0900

    🚀 [Feat]: 프로젝트 상세 모달 페이지 및 Default 모달 추가
    - 모달 페이지 `/@modal/(.)project/[projectId]/page.tsx` 생성
      - `ProjectModalType` 타입을 사용하여 프로젝트 상세 데이터를 가져오도록 구현
      - `ProjectDetail` 컴포넌트 활용, 프로젝트 세부 정보 표시
    - `/project/[projectId]/page.tsx` 추가
      - 프로젝트 상세 페이지에서 동일한 데이터를 렌더링하도록 구현
    - `/@modal/default.tsx` 기본 모달 컴포넌트 추가
      - 빈 모달 정의 및 기본 동작 오류 방지

---
+++ src/app/@modal/(.)project/[projectId]/page.tsx
@@ -0,0 +1,13 @@
+import { get } from '@/libs/fetch';
+import { ProjectModalType } from '@/types/project';
+import ProjectDetail from '@/components/ProjectDetail';
+
+interface Props {
+  params: Promise<{ projectId: string }>;
+}
+
+export default async function ProjectModal({ params }: Props) {
+  const { projectId } = await params;
+  const project = await get<ProjectModalType>(`/port/project/${projectId}`);
+  return <ProjectDetail {...project} />;
+}
