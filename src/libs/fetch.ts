ce373283c64d58a9c00f7ea61863ce9dd02a3858
kwondns <kwondns@gmail.com>
2025-07-15 01:51:41 +0900

    🚀 [Feat]: fetch 요청에 revalidate 옵션 추가
    - `fetch` 함수에 `{ next: { revalidate: 3600 } }` 옵션 추가
      - API 응답 데이터를 1시간 단위로 다시 검증하도록 설정
      - 캐싱된 데이터의 최신화를 보장하며 성능 최적화

--- src/libs/fetch.ts
+++ src/libs/fetch.ts
@@ -1,4 +1,4 @@
 export const get = async <T>(url: string): Promise<T> => {
-  const result = await fetch(`${process.env.API_URL}${url}`);
+  const result = await fetch(`${process.env.API_URL}${url}`, { next: { revalidate: 3600 } });
   return result.json();
 };
